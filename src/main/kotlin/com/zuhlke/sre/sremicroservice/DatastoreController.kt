5e3cfb16e3604ae837853ee83bb554b1b2390e5d
Natalia Oskina <natalia.oskina@zuehlke.com>
2018-06-05 10:18:39 +0200

    Add: logging in /heavyLoad, /stopHeavyLoad, mongoDB endpoints

--- src/main/kotlin/com/zuhlke/sre/sremicroservice/DatastoreController.kt
+++ src/main/kotlin/com/zuhlke/sre/sremicroservice/DatastoreController.kt
@@ -4,6 +4,7 @@ import org.litote.kmongo.KMongo
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import org.litote.kmongo.getCollection
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,24 +12,30 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DatastoreController {
+    val logger = LoggerFactory.getLogger(DatastoreController::class.java)
 
     @PostMapping("/saveUser")
     fun saveUser(user: User): User {
+        logger.debug("Reached /saveUser endpoint")
         val mongoClient = KMongo.createClient("localhost", 27017)
         val db = mongoClient.getDatabase("test")
         val col = db.getCollection<User>()
+        logger.debug("Connected to MongoDB")
         col.insertOne(user)
-
+        logger.debug("Inserted user")
         return user
     }
 
     @GetMapping("/getUserByName")
     fun getUser(@RequestParam("name") name: String): User? {
+        logger.debug("Reached /getUserByName endpoint")
         val mongoClient = KMongo.createClient("localhost", 27017)
         val db = mongoClient.getDatabase("test")
         val col = db.getCollection<User>()
-
-        return col.findOne(User :: name eq name)
+        logger.debug("Connected to MongoDB")
+        val result = col.findOne(User :: name eq name)
+        logger.debug("Retrieved user")
+        return result
     }
 
 }
--- src/main/kotlin/com/zuhlke/sre/sremicroservice/HeavyLoadController.kt
+++ src/main/kotlin/com/zuhlke/sre/sremicroservice/HeavyLoadController.kt
@@ -1,5 +1,6 @@
 package com.zuhlke.sre.sremicroservice
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,16 +9,22 @@ import java.util.concurrent.Executors
 @RestController
 class HeavyLoadController {
     var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+    val logger = LoggerFactory.getLogger(HeavyLoadController::class.java)
 
     @PostMapping("/startHeavyLoad")
     fun heavyLoad(@RequestParam("expectedCpuLoad") expectedCpuLoad: Double) {
+        logger.debug("Reached startHeavyLoad endpoint")
         if (executor.isShutdown) {
             executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+            logger.debug("Executor of threads is shutdown. Starting executor...")
         }
         HeavyLoad.load(Runtime.getRuntime().availableProcessors(), expectedCpuLoad, executor)
     }
 
     @PostMapping("/stopHeavyLoad")
-    fun stopThreads() = executor.shutdownNow()
+    fun stopThreads() {
+        logger.debug("Reached stopHeavyLoad")
+        executor.shutdownNow()
+    }
 }
 
