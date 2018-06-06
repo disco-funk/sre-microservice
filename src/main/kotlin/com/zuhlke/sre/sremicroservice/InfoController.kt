142279a5f97a2cb8b9cd68cf6575bfba621e8258
Natalia Oskina <natalia.oskina@zuehlke.com>
2018-06-06 11:33:30 +0200

    Change: log file path

--- src/main/kotlin/com/zuhlke/sre/sremicroservice/InfoController.kt
+++ src/main/kotlin/com/zuhlke/sre/sremicroservice/InfoController.kt
@@ -2,12 +2,10 @@ package com.zuhlke.sre.sremicroservice
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 class InfoController {
 
-    val counter = AtomicLong()
 
     @GetMapping("/isAlive")
     fun greeting() =
